ba5982de7a9781d1c0d91d8c39cc9d6831eb63c7
nyingi <nyingi.maina@gmail.com>
2020-05-03 20:48:28 +0300

    thing

---
+++ src/workers/preflector_files_populator.go
@@ -0,0 +1,39 @@
+package workers
+
+import (
+	"io/ioutil"
+	"path/filepath"
+)
+
+func InjectPreflectorFiles(directoryName string, preflectorFiles *[]string) {
+	items, _ := ioutil.ReadDir(directoryName)
+	for _, item := range items {
+		if item.IsDir() {
+			subitems, _ := ioutil.ReadDir(directoryName + "/" + item.Name())
+			for _, subitem := range subitems {
+				if !subitem.IsDir() {
+					currentDirectory := directoryName + "/" + item.Name()
+					handleFile(currentDirectory, subitem.Name(), preflectorFiles)
+				} else {
+					var subDirectory = directoryName + "/" + item.Name() + "/" + subitem.Name()
+					InjectPreflectorFiles(subDirectory, preflectorFiles)
+				}
+			}
+		} else {
+			handleFile(directoryName, item.Name(), preflectorFiles)
+
+		}
+	}
+}
+
+func handleFile(currentDirectory, fileName string, preflectorFiles *[]string) {
+	fullFileName := currentDirectory + "/" + fileName
+	if isPreflectorFile(fileName) {
+		*preflectorFiles = append(*preflectorFiles, fullFileName)
+	}
+}
+
+func isPreflectorFile(fileName string) bool {
+	isTargetFile, _ := filepath.Match("*.preflect.json", fileName)
+	return isTargetFile
+}
---
+++ src/workers/text_files/text_file_reader.go
@@ -0,0 +1,12 @@
+package text_files
+
+func getFileContents(file string) string {
+	/*jsonFile, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jsonFile.Close()
+	fileContents := bufio.NewScanner(jsonFile)
+	return fileContents*/
+	return "don"
+}
